540a1f40322c84487daca3b46059d0ff2ade3950
bautilazaletta <juanlazaletta@gmail.com>
2024-10-13 13:23:54 -0300

    Redireccion a /boletines

--- boletines/javascript/main.js
+++ boletines/javascript/main.js
@@ -13,7 +13,7 @@ links.forEach(function(link){
     content.classList.add("animate__animated");
 
     setTimeout(function(){
-        location.href = "/";
+        location.href = "/boletines";
     }, 600)
 
     return false;
